7cee6dbdeb68f27440b83d9114167ed2d90b55f1
diego09310 <dap09310@gmail.com>
2016-04-17 11:47:03 -0500

    minor fixes cpu

--- cpu.vhd
+++ cpu.vhd
@@ -22,23 +22,24 @@ architecture behavioral of cpu is
 signal PC : std_logic_vector (ADDRESS_WIDTH-1 downto 0) := (others => '0');
 signal IR : std_logic_vector (ADDRESS_WIDTH-1 downto 0);
 signal MDR : std_logic_vector (DATA_WIDTH-1 downto 0);
-signal registers : regs;
+signal registers : regs := (others => (others => '0'));
 signal rs, rt, rd : std_logic_vector (4 downto 0);
 signal inm : std_logic_vector (15 downto 0);
 signal inmj : std_logic_vector (25 downto 0);
 
 begin
+	IR_out <= IR;
+	PC_out <= PC;
+	MDR_out <= MDR;
+
 	process
 	begin
-		PC_out <= PC;
 		address <= PC;
 		rw_cache <= '1';
 		i_d_cache <= '1';
 		cache_enable <= '1';
 		wait until data_cache_ready='1';
-		--address <= (others => 'Z');
 		IR <= data_in;
-		IR_out <= IR;
 		cache_enable <= '0';
 		PC <= PC + 4;
 		case IR(31 downto 26) is
@@ -46,21 +47,21 @@ begin
 				rs <= IR(25 downto 21);
 				rt <= IR(20 downto 16);
 				inm <= IR(15 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				address <= (registers(to_integer(unsigned(rs))) + to_integer(unsigned(inm)));
 				rw_cache <= '1';
 				cache_enable <= '1';
 				wait until data_cache_ready='1';
 				--address <= (others => 'Z');
 				MDR <= data_in;
-				MDR_out <= MDR;
+				wait until clk='1';
 				registers(to_integer(unsigned(rt))) <= MDR;
 				cache_enable <= '0';
 			when "101011" =>		--STORE
 				rs <= IR(25 downto 21);
 				rt <= IR(20 downto 16);
 				inm <= IR(15 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				rw_cache <= '0';
 				address <= (registers(to_integer(unsigned(rs))) + to_integer(unsigned(inm)));
 				data_out <= registers(to_integer(unsigned(rt)));
@@ -72,12 +73,12 @@ begin
 					rs <= IR(25 downto 21);
 					rt <= IR(20 downto 16);
 					rd <= IR(15 downto 11);
-					wait for 1 ns; --cambiar a 1 clk?
+					wait until clk='1';
 					registers(to_integer(unsigned(rd))) <= (registers(to_integer(unsigned(rs))) + registers(to_integer(unsigned(rt))));
 
 				elsif ieee.std_logic_unsigned."=" (IR(5 downto 0), "001000") then	--JR
 					rs <= IR(25 downto 21);
-					wait for 1 ns; --cambiar a 1 clk?
+					wait until clk='1';
 					PC <= registers(to_integer(unsigned(rs)));
 				end if;
 
@@ -85,38 +86,38 @@ begin
 				rs <= IR(25 downto 21);
 				rt <= IR(20 downto 16);
 				inm <= IR(15 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				if ieee.std_logic_unsigned."=" (registers(to_integer(unsigned(rs))), registers(to_integer(unsigned(rt)))) then
-					PC <= PC + 4 + (to_integer(unsigned(inm))*4);
+					PC <= PC + (to_integer(unsigned(inm))*4);
 				end if;
 
 			when "001101" => --XORI (ALU inm)
 				rs <= IR(25 downto 21);
 				rt <= IR(20 downto 16);
 				inm <= IR(15 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				registers(to_integer(unsigned(rt))) <= registers(to_integer(unsigned(rs))) xor (x"0000" & inm);
 
 			when "000010"=>	--J
 				inmj <= IR(25 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				PC <= PC (31 downto 28) & inmj & "00";
 
 			when "001000" => --ADDI
 				rs <= IR(25 downto 21);
 				rt <= IR(20 downto 16);
 				inm <= IR(15 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				registers(to_integer(unsigned(rt))) <= (registers(to_integer(unsigned(rs))) + to_integer(unsigned(inm)));
 
 			when "001111" => --LUI
 				rt <= IR(20 downto 16);
 				inm <= IR(15 downto 0);
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				registers(to_integer(unsigned(rt))) (31 downto 16) <= inm;
 
 			when others =>
-				wait for 1 ns; --cambiar a 1 clk?
+				wait until clk='1';
 				PC <= (others => '0');
 		end case;
 	end process;
